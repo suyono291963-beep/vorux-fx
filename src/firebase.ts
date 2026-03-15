77eb5bc7e840f148b9a0b93d92c8010036665db7
suyono291963-beep <suyono291963@gmail.com>
2026-03-15 23:49:29 +0700

    add firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,17 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from '../firebase-applet-config.json';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCh0XR8xt4ddyiO_gtzDiwXa_pmvHUoV6E",
+  authDomain: "ai-studio-applet-webapp-3c969.firebaseapp.com",
+  projectId: "ai-studio-applet-webapp-3c969",
+  storageBucket: "ai-studio-applet-webapp-3c969.firebasestorage.app",
+  messagingSenderId: "688744499241",
+  appId: "1:688744499241:web:c7c51ac1b53c8f13b7f438"
+};
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
