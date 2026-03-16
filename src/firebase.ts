25b2829147fd55e8ad94bedc927c3f6946f4c29f
suyonoxxx <suyono291963@gmail.com>
2026-03-16 10:58:24 +0700

    Replace getFirestore with initializeFirestore

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCh0XR8xt4ddyiO_gtzDiwXa_pmvHUoV6E",
@@ -11,6 +11,11 @@ const firebaseConfig = {
   appId: "1:688744499241:web:c7c51ac1b53c8f13b7f438"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+
+export const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true,
+  useFetchStreams: false
+});
