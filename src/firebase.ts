8fe5e1881087125aab7235339e9d5c881b0806b2
suyonoxxx <suyono291963@gmail.com>
2026-03-16 12:07:11 +0700

    Switch to getFirestore for Firestore initialization
    Replaced initializeFirestore with getFirestore and removed Firestore configuration options.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { initializeFirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCh0XR8xt4ddyiO_gtzDiwXa_pmvHUoV6E",
@@ -14,8 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-
-export const db = initializeFirestore(app, {
-  experimentalForceLongPolling: true,
-  useFetchStreams: false
-});
+export const db = getFirestore(app);
